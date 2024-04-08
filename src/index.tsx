12fea22c4cdac65b0dccb51286fab035bd419daf
Ashish Patel <aashish.patels22@gmail.com>
2024-04-08 22:13:00 +0530

    Jbook Project Initial commit Having load nested packages

---
+++ src/index.tsx
@@ -0,0 +1,56 @@
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from "react";
+import { createRoot } from "react-dom/client";
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+const App = () => {
+    const ref = useRef<esbuild.Service>();
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        });
+    };
+
+    useEffect(() => {
+        startService();
+    }, []);
+
+
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+
+        // const result = await ref.current.transform(input, {
+        //     loader: 'jsx',
+        //     target: 'es2015'
+        // });
+
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()]
+        });
+
+        //console.log(result);
+        setCode(result.outputFiles[0].text);
+    };
+
+    return (<div>
+        <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+        <div>
+            <button onClick={onClick}>Submit</button>
+        </div>
+        <pre>{code}</pre>
+    </div>
+    );
+};
+
+root.render(<App />);
