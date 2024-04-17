91c14bd4755126c20b525d2853ecbf55c01c6f04
Ashish Patel <aashish.patels22@gmail.com>
2024-04-17 22:53:04 +0530

    94. Breaking Up Resolve Logic With Filters

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,29 +6,26 @@ const fileCache = localforage.createInstance({
   name: 'filecache'
 });
 
-(async () => {
-  await fileCache.setItem('color', 'red');
-
-  const color = await fileCache.getItem('color');
-  console.log(color);
-})()
-
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: esbuild.OnResolveArgs) => {
-        console.log('onResolve', args);
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        }
 
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL(args.path, 'https://www.unpkg.com' + args.resolveDir + '/').href
-          }
+      //Handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js)/ }, () => {
+        return { path: 'index.js', namespace: 'a' };
+      });
+
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: esbuild.OnResolveArgs) => {
+        return {
+          namespace: 'a',
+          path: new URL(args.path, 'https://www.unpkg.com' + args.resolveDir + '/').href
         }
+      });
+
+      // Handle main file of a module
+      build.onResolve({ filter: /.*/ }, async (args: esbuild.OnResolveArgs) => {
 
         return { namespace: 'a', path: `https://www.unpkg.com/${args.path}` };
 
@@ -38,26 +35,35 @@ export const unpkgPathPlugin = () => {
 
       });
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
+      build.onLoad({ filter: /.*/ }, async (args: esbuild.OnLoadArgs) => {
 
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import React, {useState} from 'react-select';
-              console.log(useState);
-            `,
+            contents: inputCode,
           };
         }
 
-        const { data, request } = await axios.get(args.path);
+        // check to see if we have already fecthed this file
+        // and if it is in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 
-        return {
+        // if it is, return it immediately
+        if (cachedResult) {
+          return cachedResult;
+        }
+
+        const { data, request } = await axios.get(args.path);
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname
         };
+
+        // store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
 
       });
     },
